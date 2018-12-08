6ec61b5251906b5e1dd06748486ee0c3a93e28a1
Marco G. Maia <marcogmaia@gmail.com>
2018-12-08 17:46:06 -0300

    refactor

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -9,7 +9,12 @@ import React, { Component } from "react";
 class Landing extends Component {
   // imgfolder = "../epubs/covers/";
   state = {
-    image: ["gatsby", "got", "harryPotter", "stephenKing"]
+    image: [
+      { id: 1, value: "gatsby" },
+      { id: 2, value: "got" },
+      { id: 3, value: "harryPotter" },
+      { id: 4, value: "stephenKing" }
+    ]
   };
 
   render() {
@@ -20,14 +25,14 @@ class Landing extends Component {
             {this.state.image.map(img => (
               <div className="card">
                 <img
+                  key={img.id}
                   height="400px"
-                  src={require(`../epubs/covers/${img}.jpg`)}
+                  src={require(`../epubs/covers/${img.value}.jpg`)}
                   alt=""
                 />
               </div>
             ))}
           </div>
-
           <div className="container">
             <h1>pronto agora vai</h1>
           </div>
