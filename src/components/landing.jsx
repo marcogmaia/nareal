7f06a57118e7a9ed4ef38378bdb27b0b023e77c5
Marco G. Maia <marcogmaia@gmail.com>
2018-12-08 17:39:16 -0300

    automatic images from folder

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -17,45 +17,13 @@ class Landing extends Component {
       <div>
         <div className="album text-muted">
           <div className="row">
-            <div className="card">
-              <img
-                height="500px"
-                src={require("../epubs/covers/gatsby.jpg")}
-                alt=""
-              />
-              <p className="card-text text-center">mas que coisa doido</p>
-            </div>
-            <div className="card">
-              <img
-                height="500px"
-                src={require("../epubs/covers/got.jpg")}
-                alt=""
-              />
-            </div>
-            <div className="card">
-              <img
-                height="500px"
-                src={require("../epubs/covers/harryPotter.jpg")}
-                alt=""
-              />
-              <div className="card" />
-            </div>
-            <div>
-              <img
-                height="500px"
-                src={require("../epubs/covers/stephenKing.jpg")}
-                alt=""
-              />
-            </div>
+            {this.state.image.map(img => (
+              <div className="card">
+                <img src={require(`../epubs/covers/${img}`)} alt="" />
+              </div>
+            ))}
           </div>
-          {/* <ul>
-          <li>
-            <img src={require("../epubs/covers/harryPotter.jpg")} alt="" />
-          </li>
-          <li>
-            <img src={require("../epubs/covers/stephenKing.jpg")} alt="" />
-          </li>
-        </ul> */}
+
           <div className="container">
             <h1>pronto agora vai</h1>
           </div>
